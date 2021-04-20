92be3e2cb6f8c31933b95751f187c6288c0e2e8f
chris <chris@net-island.de>
2021-04-20 17:40:29 +0200

    upgrade sshtunnel, otherwise threads might keep dangling for invalid ssh configs

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='tap-postgres',
           'psycopg2==2.7.4',
           'strict-rfc3339==0.7',
           'nose==1.3.7',
-          'sshtunnel==0.1.5',
+          'sshtunnel>=0.4.0',
           'paramiko==2.7.2'
       ],
       entry_points='''
